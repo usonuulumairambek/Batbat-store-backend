dab1672a9fa06dbfbe7387bd8d37dc04f81511c9
My name <iakylbek005@gmail.com>
2021-10-13 15:40:12 +0600

    auto doc

--- Bat_Bat_Store/settings.py
+++ Bat_Bat_Store/settings.py
@@ -31,13 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Shope'
+    # apps
+    'Shope.apps.ShopeConfig',
+
+    'drf_yasg',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
