492af3954b9a4e1b94a1343d586e906c59b6a7ed
My name <iakylbek005@gmail.com>
2021-10-15 17:26:09 +0600

    change models

--- Shope/models.py
+++ Shope/models.py
@@ -8,6 +8,8 @@ class Category(models.Model):
 
     class Meta:
         ordering = ('orderId', )
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return f'{self.category}'
@@ -32,10 +34,17 @@ class Product(models.Model):
     in_line = models.IntegerField()
     lines = models.IntegerField()
     discount = models.DecimalField(max_digits=20, decimal_places=0, null=True)
+    cloth = models.CharField(max_length=64)
+    manufacturer_country = models.CharField(max_length=32)
     orderId = models.IntegerField(null=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
 
     class Meta:
         ordering = ('orderId', )
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
     def __str__(self):
         return f'{self.title}'
@@ -48,6 +57,8 @@ class Image(models.Model):
 
     class Meta:
         ordering = ('orderId', )
+        verbose_name = 'Изображение продукта'
+        verbose_name_plural = 'Изображение продуктов'
 
     def __str__(self):
         return f'{self.product} {self.image}'
