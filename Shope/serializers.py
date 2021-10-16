265471420f254a866aa48285c53afc848386c8ef
Hikki <a.aidaraliev25@gmail.com>
2021-10-16 15:38:25 +0600

    product

---
+++ Shope/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from Shope.models import Product, Image
+from django.contrib.auth.models import User
+
+
+class ProductSerializer(ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ('id', 'category', 'title', 'description', 'price', 'color',
+                  'size', 'seller', 'in_line', 'lines', 'discount', 'favorite',)
+
+
+class FavoriteProductSerializer(ModelSerializer):
+    user_id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Image
+        fields = ('id', 'product', 'user_id')
