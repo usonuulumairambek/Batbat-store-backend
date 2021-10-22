447c63447f419ae1c10ab8785f25b16ae9450075
Hikki <a.aidaraliev25@gmail.com>
2021-10-22 15:33:43 +0600

    add postgres database

--- Bat_Bat_Store/settings.py
+++ Bat_Bat_Store/settings.py
@@ -84,12 +84,18 @@ WSGI_APPLICATION = 'Bat_Bat_Store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bat_bat',
+        'USER': 'adilet',
+        'PASSWORD': 'adi',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
